465a902432d9d1e493acad303464fcc98f4663b1
StevenWon <335891379@qq.com>
2021-09-01 17:50:31 +0800

    用 Node.js写一个小的服务端

---
+++ 第一周作业/server.js
@@ -0,0 +1,17 @@
+///用Node.js编写一个服务端
+const http = require("http")
+http.createServer((request, response) => {
+    let body = [];  
+    request.on('error', (err) => {
+        console.error(err); 
+    }).on('data', (chunk) => {
+        body.push(chunk.toString());  
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('body:', body);
+        response.writeHead(200, {'Content-Type':'text/html'})
+        response.end(' Hello World\n')
+    });
+}).listen('8080');
+
+console.log('server started');
